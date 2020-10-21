324c47baeaefee7d26b4dc3fa5443c0491c9ca06
Cristian Garcia <cg7497@gmail.com>
2020-10-21 08:31:21 -0500

    created blog post .java

---
+++ src/main/java/com/tts/ttsblog/BlogPost/BlogPost.java
@@ -0,0 +1,67 @@
+package com.tts.ttsblog.BlogPost;
+
+public class BlogPost {
+// Properties of the blog post
+
+private Long id;
+private String title;
+private String author;
+private String blogEntry;
+
+
+// default constructor
+public BlogPost(){ 
+
+}
+public BlogPost(String title, String author, String blogEntry){ 
+this.title = title;
+this.author = author;
+this.blogEntry = blogEntry;
+}
+//instatiate contructor in controller with   BlogPost blogpost = new BlogPost();
+public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getTitle() {
+    return title;
+}
+
+public void setTitle(String title) {
+    this.title = title;
+}
+
+public String getAuthor() {
+    return author;
+}
+
+public void setAuthor(String author) {
+    this.author = author;
+}
+
+public String getBlogEntry() {
+    return blogEntry;
+}
+
+public void setBlogEntry(String blogEntry) {
+    this.blogEntry = blogEntry;
+}
+
+@Override
+public String toString() {
+    return "BlogPost [author=" + author + ", blogEntry=" + blogEntry + ", title=" + title + "]";
+}
+
+
+
+
+
+
+}
+
+
+//instatiate contructor in controller with   BlogPost blogpost = new BlogPost();
