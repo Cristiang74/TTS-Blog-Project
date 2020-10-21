ddaad5c1d2bcefeb4681bc7b7869f8fe47a0487f
Cristian Garcia <cg7497@gmail.com>
2020-10-21 09:09:33 -0500

    added blog post

---
+++ src/main/java/com/tts/ttsblog/BlogPost/BlogPostController.java
@@ -0,0 +1,18 @@
+package com.tts.ttsblog.BlogPost;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogPostController {
+
+    @GetMapping(value = "/")
+    public String index(BlogPost blogPost){
+        return "blogpost/index";
+    }
+
+
+
+
+
+}
