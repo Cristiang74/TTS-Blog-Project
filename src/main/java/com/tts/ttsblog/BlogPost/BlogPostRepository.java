31a43f2029e4e6b18dbf19c914d97fd9d63e592d
Cristian Garcia <cg7497@gmail.com>
2020-10-21 09:13:49 -0500

    created a blog repository

---
+++ src/main/java/com/tts/ttsblog/BlogPost/BlogPostRepository.java
@@ -0,0 +1,13 @@
+package com.tts.ttsblog.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+
+
+
+
+
+
+}
