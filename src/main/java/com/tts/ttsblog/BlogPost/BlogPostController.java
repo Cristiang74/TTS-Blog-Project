a4c80120bfd44bb0859ec5619ab85af0aaa37b77
Cristian Garcia <cg7497@gmail.com>
2020-11-01 12:03:19 -0600

    added get mapping to new blogpost pages

--- src/main/java/com/tts/ttsblog/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/ttsblog/BlogPost/BlogPostController.java
@@ -6,8 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogPostController {
@@ -21,15 +25,28 @@ public class BlogPostController {
     //localhost:8080
     //Handle get request to forward slash
     @GetMapping(value = "/")
-    public String index(BlogPost blogPost){
+    public String index(BlogPost blogPost, Model model){
+        posts.removeAll(posts);
+        
+        for (BlogPost postFromDB : blogPostRepository.findAll()) {
+            posts.add(postFromDB);
+        }
+        model.addAttribute("posts", posts);
         return "blogpost/index";
     }
 
+    @GetMapping(value="/blogpost/new")
+    public String newBlog(BlogPost blogPost){
+        return "blogpost/new";
+    }
 
-    @PostMapping(value = "/")
+    @PostMapping(value = "/blogpost")
     public String addNewBlogPost(BlogPost blogPost, Model model){
-        
-        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+
+       
+        System.out.println(posts);
+
+        blogPostRepository.save(blogPost);
 
         model.addAttribute("title", blogPost.getTitle());
         model.addAttribute("author", blogPost.getAuthor());
@@ -38,6 +55,13 @@ public class BlogPostController {
 
     }
 
+    @RequestMapping(value = "/blogpost/{id}", method = RequestMethod.DELETE)
+    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost){
+        
+
+        blogPostRepository.deleteById(id);
+        return "/blogpost/index";
+    }
 
 
 }
